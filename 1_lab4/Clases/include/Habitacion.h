718591ada54363537c6a7c55eefcb1f174aa6600
Danilo <danilogiacometti21@gmail.com>
2022-06-10 13:15:18 -0300

    operaciones, faltan cosas

--- 1_lab4/Clases/include/Habitacion.h
+++ 1_lab4/Clases/include/Habitacion.h
@@ -7,8 +7,8 @@
 
 class Habitacion{
     private:
-        int numero=0, capacidad=0;
-        float precio=0.0;
+        int Numero=0, Capacidad=0;
+        float Precio=0.0;
         Reserva* res;
     public:
         Habitacion();
@@ -21,6 +21,24 @@ class Habitacion{
         void setCap(int capacidad);
         void setPrecio(float precio);
         void setReserva(Reserva *res);
+
+        //Operaciones
+        bool habitacionLibre(In,Out);//hay que modificar cosas, falta implementar coleeciones
+
+        DTHabitacion getDTHabitacion();
+
+        //DTReserva* darReservasHuespedHab(email);
+        
+        //reservasHabitacion():set(Reservas) creo que es la misma que la de getReserva
+
+        //bool mismoNumero(nr); Esta operacion esta mal desde el diagrama de comunicacion, no tiene sentido
+
+        //DTHuesped* obtenerHuesprResHab(nr);
+
+        bool buscarReserva(cres);
+        void eliminarLinkRes();
+
+        //Destructora
         ~Habitacion();
 };
 
--- 1_lab4/Clases/src/Habitacion.cpp
+++ 1_lab4/Clases/src/Habitacion.cpp
@@ -6,21 +6,21 @@ Habitacion:: Habitacion(){
 }
 
 Habitacion:: Habitacion(int numero, int capacidad,float precio){
-    this->numero=numero;
-    this->capacidad=capacidad;
-    this->precio=precio;
+    this->Numero=numero;
+    this->Capacidad=capacidad;
+    this->Precio=precio;
 }
 
 int Habitacion::getNum(){
-        return this->numero;
+        return this->Numero;
  }
 
 int Habitacion::getCap(){
-    return this->capacidad;
+    return this->Capacidad;
 }
 
 float Habitacion::getPrecio(){
-        return this->precio;
+        return this->Precio;
 }
 
 Reserva* Habitacion::getReserva(){
@@ -28,20 +28,46 @@ Reserva* Habitacion::getReserva(){
 }
 
 void Habitacion::setNum(int numero){
-    this->numero=numero;
+    this->Numero=numero;
 }
 
 void Habitacion::setCap(int capacidad){
-    this->capacidad=capacidad;
+    this->Capacidad=capacidad;
 }
 
 void Habitacion::setPrecio(float precio){
-    this->precio=precio;
+    this->Precio=precio;
 }
 
-  void Habitacion::setReserva(Reserva *res){
-      this->res = res;
-  }
+void Habitacion::setReserva(Reserva *res){
+  this->res = res;
+}
+
+
+//hay que modificar cosas, falta implementar coleeciones
+bool habitacionLibre(In,Out){
+    int maxReservas = 0; //lo hago solo para que compile, representa el numero de reservas asociadas a una habitacion
+    bool libre = true;
+    int i = 0;
+    while(i <= maxReservas && libre){ 
+        if(this->res.getCheckIn() < Out || this->res.getCheckOut() > In){
+            libre = false;
+        }//pruebo esto para cada reserva de la habitacion
+    }
+    return libre;
+}
+
+DTHabitacion getDTHabitacion(){
+    return DTHabitacion(this->Numero, this->Capacidad, this->Precio);
+}
+
+bool buscarReserva(cres){
+    return this->res.getCodigo() == cres;
+}
+
+void eliminarLinkRes(){
+    this->res == NULL;
+}
 
 Habitacion::~Habitacion(){
 }
