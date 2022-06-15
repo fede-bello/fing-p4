48e9a68fe89d6825d3d0263600ba62ffe4b6d018
diego.amorena <diego.amorena@fing.edu.uy>
2022-06-14 22:22:36 -0300

    main

--- 1_lab4/main.cpp
+++ 1_lab4/main.cpp
@@ -14,38 +14,38 @@
 void ImprimirOpciones()
 {
     cout << endl;
-    cout << "Ingresar opción:" << endl;
+    cout << "Ingresa la opción deseada" << endl;
     cout << "0 -> Salir" << endl;
     cout << "1 -> Alta de Usuario " << endl;
     cout << "2 -> Alta de Hostal" << endl;
     cout << "3 -> Alta de Habitacion" << endl;
     cout << "4 -> Asignar empleado a hostal" << endl;
     cout << "5 -> Realizar Reserva" << endl;
-    cout << "5 -> Consultar top 3 de hostales" << endl;
-    cout << "6 -> Registrar Estadia" << endl;
-    cout << "7 -> Finalizar Estadia" << endl;
-    cout << "8 -> Calificar Estadia" << endl;
-    cout << "9 -> Comentar Calificacion" << endl;
-    cout << "10 -> Consulta de Usuario" << endl;
-    cout << "11 -> Consulta de Hostal" << endl;
-    cout << "12 -> Consulta de Reserva" << endl;
-    cout << "13-> Consulta de Estadia" << endl;
-    cout << "14 -> Baja de Reserva" << endl<<endl;
+    cout << "6 -> Consultar top 3 de hostales" << endl;
+    cout << "7 -> Registrar Estadia" << endl;
+    cout << "8 -> Finalizar Estadia" << endl;
+    cout << "9 -> Calificar Estadia" << endl;
+    cout << "10 -> Comentar Calificacion" << endl;
+    cout << "11 -> Consulta de Usuario" << endl;
+    cout << "12 -> Consulta de Hostal" << endl;
+    cout << "13 -> Consulta de Reserva" << endl;
+    cout << "14-> Consulta de Estadia" << endl;
+    cout << "15 -> Baja de Reserva" << endl<<endl;
 }
 
 
 
 int main()
-{    
-	while(true){
+{   bool iterarWhile=true;
+    cout << "Bienvenido" << endl;
+	while(iterarWhile){
         ImprimirOpciones();
 		int FuncionSelecionada = 0; 
 		cin >> FuncionSelecionada;
 		switch (FuncionSelecionada) {
-                case 1:{  
+                case 1:{  //ALTA USUARIO
                     controladorUsuario *cUsuario=controladorUsuario::getInstance();
-                    int esEmpleado;
-                    cout << "Alta de Usuario"  << endl;        
+                    cout << "Alta Usuario"  << endl;        
                     cout<< "Digite su nombre"<<endl;
                     string nombre;
                     cin>>nombre;
@@ -56,6 +56,7 @@ int main()
                     string mail;
                     cin>>mail;
                     cout<< "Digite 1 si es un Empleado, de lo contrario 0"<<endl;
+                    int esEmpleado;
                     cin>>esEmpleado;
                     if(esEmpleado){
                         int TrabajoSeleccionado;
@@ -79,8 +80,10 @@ int main()
                             case 4:
                                 cargoEmpleado=Infrasetructura;
                                 break;}
-                            try{
+                                //EMPÏEZO A LLAMAR A LAS FUNCIONES DEL CONTROLADOR
                                 DTEmpleado *dtusuario=cUsuario->NuevoEmpleado(mail,password,nombre,cargoEmpleado);
+                            try{
+                                
                             }catch(...){
                                 cout<< "Hay otro usuario con ese mail"<<endl;
                             }
@@ -98,12 +101,10 @@ int main()
                             
                         }
                     }
-                
-                    break;//fin case 1
-                case 2:{
+                    break;//FIN ALTA USUARIO 
+                case 2:{ //ALTA DE HOSTAL
                     cout << "Alta de Hostal"  << endl;
                     bool salir=true;
-                    while(salir)
                     cout<< "Digite el nombre del Hostal"<<endl;
                     string nombre;
                     cin>>nombre;
@@ -121,14 +122,52 @@ int main()
                     catch(...){
                         cout<< "Ya hay un hostal con ese nombre"<<endl;
                     }
-                    
-                }
+                }//FIN ALTA HOSTAL
+                    break;
+                case 3:{//ALTA DE HABITACION
+                    controladorHostal *cHostal=controladorHostal::getInstance();
+
+                }//FIN ALTA HABITACION
+                    break;
+                    case 4:{//ASIGNAR EMPLEADO A HOSTAL
+                }//FIN ASIGNAR EMPLEADO A HOSTAL
+                    break;
+                    case 5:{//REALIZAR RESERVA
+                }//FIN REALIZAR RESERVA
+                    break;
+                    case 6:{//CONSULTA TOP 3 HOSTALES
+                }//FIN CONSULTA TOP 3 HOSTALES
+                    break;
+                    case 7:{// REGISTRAR ESTADIA
+                }//FIN REGISTRAR ESTADIA
+                    break;
+                    case 8:{// FINALIZAR ESTADIA
+                }//FIN FINALIZAR ESTADIA
+                    break;
+                    case 9:{// CALIFICAR ESTADIA
+                }//FIN CALIFICAR ESTADIA
+                    break;
+                    case 10:{// COMENTAR CALIFICACION
+                }//FIN COMENTAR CALIFICACION
+                    break;
+                    case 11:{// CONSULTA USUARIO
+                }//FIN CONSULTA USUARIO
+                    break;
+                    case 12:{// CONSULTA DE HOSTAL
+                }//FIN CONSULTA DE HOSTAL
+                    break;
+                    case 13:{// CONSULTA DE RESERVA
+                }//FIN CONSULTA DE RESERVA
+                    break;
+                    case 14:{// CONSULTA DE ESTADIA
+                }//FIN CONSULTA DE ESTADIA
+                    break;
+                    case 15:{// BAJA RESERVA
+                }// FIN BAJA RESERVA
                     break;
                 default:{
-
-                }
-                    break;
-                
+                    iterarWhile=false;
+                }  
             }//end switch
 	    }//end while
 	return 0;
